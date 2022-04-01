70c518f1de1190a201c41437f864bda65e81dce7
Tao Tien <29749622+taotien@users.noreply.github.com>
2022-04-01 01:32:09 -0700

    rewrite

--- src/python/pactl.py
+++ src/python/pactl.py
@@ -1,31 +1,42 @@
-import subprocess as subprocess
+import subprocess
 from collections import defaultdict
 
-def list_sink_inputs():
+def list_sink_inputs(option='a'):
+    arg = 'media.name' if option=='m' else 'application.name'
+    try:
+        pactl = subprocess.check_output(['pactl', 'list', 'sink-inputs']).decode('utf-8')
+    except subprocess.CalledProcessError as ex:
+        return
+    lines = [i.strip() for i in pactl.splitlines() if 'Sink Input' in i or arg in i]
+    if option == 'm':
+        names = [i[14:-1] for i in lines[1::2]]
+    else:
+        names = [i[20:-1] for i in lines[1::2]]
+    nums = [i[12:] for i in lines[::2]]
+    return names, nums
+
+    
+def dict_apps():
     """
     Returns a defaultdict of application names for keys and sink numbers for
     values.
 
     For example:
-    >>> list_sink_inputs()
+    >>> dict_apps()
     defaultdict(<class 'list'>, {'WEBRTC VoiceEngine': [5], 'java': [2828], 'Firefox': [5168, 5175]})
     
     There can be multiple id's per application, such as for Firefox, where each
     tab has its own audio stream.
     """
     sinks = defaultdict(list)
-    pactl = subprocess.Popen(['pactl', 'list', 'sink-inputs'], stdout=subprocess.PIPE)
-    outval = pactl.communicate()[0].decode('utf-8')
-    lines = [i.strip() for i in outval.splitlines() if 'Sink Input' in i or 'application.name' in i]
-    names = lines[1::2]
-    numbs = lines[::2]
+    names, nums = list_sink_inputs('a')
     for i in range(len(names)):
-        sinks[names[i][20:-1]].append(numbs[i][12:])
+        sinks[names[i]].append(nums[i])
     return sinks
 
 
 def set_sink_input_vol(sink, vol):
-    subprocess.Popen(['pactl', 'set-sink-input-volume', sink, str(vol)+'%'], stdout=subprocess.PIPE)
+    return subprocess.call(['pactl', 'set-sink-input-volume', sink, str(vol)+'%'])
     
 
 def set_sink_default_mute(action):
@@ -42,3 +53,29 @@ def set_source_default_mute(action):
               'toggle'
               )
     subprocess.Popen(['pactl', 'set-source-mute', '@DEFAULT_SOURCE@', action])
+    
+
+def set_active_sink_input_vol(vol):
+    try:
+        win_id = subprocess.check_output(['xdotool', 'getactivewindow']).decode('utf-8')
+        win_info = subprocess.check_output(['xwininfo', '-id', win_id]).decode('utf-8')
+    except subprocess.CalledProcessError as ex:
+        return
+    
+    win_name = win_info.splitlines()[1][32:-1]
+    try:
+        win_name = win_name[:win_name.rindex(' — ')]
+    except ValueError as ex:
+        pass
+    print(win_name)
+    
+    names, nums = list_sink_inputs('m')
+    print(names)
+    ret = 1
+    try:
+        i = names.index(win_name)
+    except ValueError as ex:
+        pass
+    ret = set_sink_input_vol(nums[i], vol)
+
+    return ret
