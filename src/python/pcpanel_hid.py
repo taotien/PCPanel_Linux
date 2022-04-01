f00ab1384ae207baad32d14c3909e3b9bb79dd56
Tao Tien <29749622+taotien@users.noreply.github.com>
2022-04-01 01:40:04 -0700

    rewrite

--- src/python/pcpanel_hid.py
+++ src/python/pcpanel_hid.py
@@ -1,43 +1,7 @@
 #!/usr/bin/python3
 
 import hid
-import subprocess as sp
-import time
-
-
-_pactl_sinks_dict_fresh = False
-_pactl_sinks_dict_time = time.time() - 1
-_pactl_sinks = {}
-def pactl_sinks_dict():
-    global _pactl_sinks_dict_fresh
-    global _pactl_sinks_dict_time
-    global _pactl_sinks
-    
-
-    if (_pactl_sinks_dict_time + 2) < time.time():
-        _pactl_sinks_dict_time = time.time()
-        _pactl_sinks_dict_fresh = False
-        return _pactl_sinks
-        
-    sinks = _pactl_sinks
-    if not _pactl_sinks_dict_fresh:
-        pactl = sp.Popen(['pactl', 'list', 'sink-inputs'], stdout=sp.PIPE)
-        outval = pactl.communicate()[0].decode('utf-8')
-        lines = [i.strip() for i in outval.splitlines() if 'Sink Input' in i or 'application.name' in i]
-        sinks = {'Firefox': [], 'Discord': []}
-        try:
-            for i in range(int(len(lines)/2)):
-                sinks[lines[i*2+1].strip('application.name = "').strip('"')].append(int(lines[i*2].strip('Sink Input #')))
-        except KeyError:
-            pass
-        _pactl_sinks_dict_fresh = True
-
-    _pactl_sinks = sinks
-    return sinks
-
-def pactl_set(sink, vol):
-    __ = sp.Popen(['pactl', 'set-sink-input-volume', str(sink), str(vol)+'%'], stdout=sp.PIPE)
-
+import pactl
 
 class PCPanel:
     
@@ -47,13 +11,13 @@ class PCPanel:
         self.pcpanel.set_nonblocking(1)
         self.K1 = ''
         self.K2 = ''
-        self.K3 = 'Mic'
+        self.K3 = ''
         self.K4 = ''
         self.K5 = ''
-        self.S1 = 'Discord'
+        self.S1 = 'WEBRTC VoiceEngine'
         self.S2 = 'Firefox'
         self.S3 = ''
-        self.S4 = 'Game'
+        self.S4 = ''
     def __del__(self):
         self.pcpanel.close()
     def __data(self):
@@ -88,19 +52,12 @@ class PCPanel:
             val = data[2]
             if (dial == ''):
                 dial == 'Other'
-            for sink in pactl_sinks_dict().get(dial):
-                pactl_set(sink, int(val/255*100))
+            for sink in pactl.dict_apps().get(dial):
+                pactl.set_sink_input_vol(sink, int(val/255*100))
 
 
 
 if __name__ == "__main__":
-    __ = pactl_sinks_dict()
     p = PCPanel()
-    while(True):
-        try:
-            p.loop()
-        except KeyboardInterrupt:
-            del p
-
-    pactl_sinks_dict()
-  
+    while True:
+        p.loop()
