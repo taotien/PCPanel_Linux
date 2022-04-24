2474f72fad49f4d05b4ddf9fd3fbd13c7f375319
Tao Tien <29749622+taotien@users.noreply.github.com>
2022-04-23 22:52:48 -0700

    rename

--- src/python/main.py
+++ src/python/main.py
@@ -1,54 +1,10 @@
 #!/usr/bin/python3
 
 import PCPanel
+import config
 
 if __name__ == "__main__":
-    p = PCPanel.PCPanel(inputs = {
-        'K1':(
-            ),
-        'B1':(
-            ),
-        'K2':(
-            ),
-        'B2':(
-            ),
-        'K3':(
-            ),
-        'B3': (
-            [],
-            [],
-            False,
-            False,
-            True),
-        'K4':(),
-        'B4':(),
-        'K5':(),
-        'B5':(),
-        'S1': (
-            ['WEBRTC VoiceEngine'], 
-            [], 
-            False, 
-            False, 
-            False),
-        'S2': (
-            ['Firefox'], 
-            [], 
-            False, 
-            False, 
-            False),
-        'S3': (
-            ['mpv', 'cmus'], 
-            [], 
-            False, 
-            False, 
-            False),
-        'S4': (
-            ['java', 'Apex Legends'],
-            [], 
-            True, 
-            False, 
-            False)
-        })
+    p = PCPanel.PCPanel(config.inputs)
     try:
         while True:
             p.loop()
