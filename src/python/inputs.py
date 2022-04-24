64c2991f90e8becc48a546e9ec2e43624c37592d
Tao Tien <29749622+taotien@users.noreply.github.com>
2022-04-23 22:49:32 -0700

    now user config is in its own file

---
+++ src/python/inputs.py
@@ -0,0 +1,46 @@
+inputs = {
+        'K1':(
+            ),
+        'B1':(
+            ),
+        'K2':(
+            ),
+        'B2':(
+            ),
+        'K3':(
+            ),
+        'B3': (
+            [],
+            [],
+            False,
+            False,
+            True),
+        'K4':(),
+        'B4':(),
+        'K5':(),
+        'B5':(),
+        'S1': (
+            ['WEBRTC VoiceEngine'], 
+            [], 
+            False, 
+            False, 
+            False),
+        'S2': (
+            ['Firefox'], 
+            [], 
+            False, 
+            False, 
+            False),
+        'S3': (
+            ['mpv', 'cmus'], 
+            [], 
+            False, 
+            False, 
+            False),
+        'S4': (
+            ['java', 'Apex Legends'],
+            [], 
+            True, 
+            False, 
+            False)
+        }
